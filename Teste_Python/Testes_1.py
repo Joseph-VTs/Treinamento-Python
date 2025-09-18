41e2b5c3f23265cbbf90cffbc472a49e28f1971a
Joseph-VTs <compcopycode@gmail.com>
2025-09-18 15:43:01 -0300

    Create Testes_1.py
    Primeiros testes

---
+++ Teste_Python/Testes_1.py
@@ -0,0 +1,32 @@
+# Comentários
+print("Olá Mundo! Python\n") # = Olá Mundo! Python
+
+# Tipos = type
+Type_Boolean = False
+print(type(Type_Boolean), "Tipo Boolena")
+Type_String1 = "String Aspas Duplas"
+print(type(Type_String1), "String Aspas Duplas")
+Type_String2 = 'String Aspas Simples'
+print(type(Type_String2), 'String Aspas Simples')
+Type_String3 = "854"
+print(type(Type_String3), "854")
+Type_Float1 = 3,5 # Tupúla
+# Acrescentando o F para formatar e usar o sinal de "+" como concatenação
+print(f"v1" + str(type(Type_Float1)), 3,5, str("Vírgula" + " " + "Separa kkk"))
+print("v2", type(Type_Float1), 3,5, "v2")
+Type_Float2 = 3.5
+print(type(Type_Float2), 3.5, "Float")
+Type_Int = 34
+print(type(Type_Int), 34, "Inteiro\n")
+
+Numeros1 = "2"
+Numeros2 = "4"
+# Junta os dos pois são Strings
+print(Numeros1 + Numeros2) # = 24
+
+Palavra1 = 'Ana"s' # = Ana"s
+print(Palavra1)
+Palavra1 = "Ana's" # = Ana's
+print(Palavra1)
+Palavra1 = "Ana\"s" # = Ana"s
+print(Palavra1 + "\n")
