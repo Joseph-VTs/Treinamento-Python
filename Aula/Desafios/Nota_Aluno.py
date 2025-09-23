76185bea152a10524346b320301488a4b365c908
Joseph-VTs <compcopycode@gmail.com>
2025-09-22 21:47:42 -0300

    Create Nota_Aluno.py
    Desafio em Python

---
+++ Aula/Desafios/Nota_Aluno.py
@@ -0,0 +1,6 @@
+Nota_1 = float(input("1° Nota do Aluno: "))
+Nota_2 = float(input("2° Nota do Aluno: "))
+
+Media_Nota = (Nota_1 + Nota_2) / 2
+
+print(f'Média do Aluno entre {Nota_1} e {Nota_2} = {Media_Nota : .1f}')
