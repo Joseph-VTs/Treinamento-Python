5b73d5ecf3b1050282c40798f9ea5aa338db5a9b
Joseph-VTs <compcopycode@gmail.com>
2025-09-22 21:47:50 -0300

    Create Operadores.py
    Desafio em Python

---
+++ Aula/Desafios/Operadores.py
@@ -0,0 +1,6 @@
+Numero = float(input("Digite um Valor: "))
+Raiz = float(0.5)
+Dobro = float(Numero * 2)
+Triplo = float(Numero * 3)
+
+print(f'\nDobro de {Numero} = {Dobro : .2f} \nTriplo de {Numero} = {Triplo : .2f} \nRaiz Quadrada de {Numero} = {Numero ** Raiz:.2f}')
