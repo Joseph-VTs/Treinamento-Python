6721af9a2ffebf9b63dd7bd496d8ee469cf06cba
Joseph-VTs <compcopycode@gmail.com>
2025-09-22 21:47:33 -0300

    Create Antes_Depois.py
    Desafio em Python

---
+++ Aula/Desafios/Antes_Depois.py
@@ -0,0 +1,5 @@
+Numero_Int = int(input('Digite um Número Inteiro: '))
+Numero_Float = float(input('Digite um Número com Float: '))
+
+print(f'\nValor Digitado: {Numero_Int} \nNúmero Antes: {Numero_Int - 1 : .2f} \nNúmero Depois: {Numero_Int + 1 : .2f}')
+print(f'\nValor Digitado: {Numero_Float : .2f} \nNúmero Antes: {Numero_Float - 1 : .2f} \nNúmero Depois: {Numero_Float + 1 : .2f}')
